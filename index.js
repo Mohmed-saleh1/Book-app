0f2c4295e9fd33047c156052f113b18480d99b34
Mohmed-saleh1 <mu.saleh616@gmail.com>
2023-09-14 17:19:51 +0300

    creating server using express

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
-const http = require('http')
+const express = require('express')
+const app = express();
 
-const books = [{name:"ahmed",age:22},{name:"Mohamed",age:23}]
-const server = http.createServer((req,res)=>{
-    if(req.url==="/"){
-        res.write(" <h1> ya welcome ya welcome :) </h1> ")
-        res.end()
-    }
-    if (req.url==="/api/books") {
-        res.write(JSON.stringify(books))
-        res.end()
-    }
-});
+const books = [
+    {name:"ahmed",age:21},
+    {name:"Mohamed",age :22}
+]
+app.get('/',(req,res)=>{
+    res.json(books)
+})
 const port = 4000
-server.listen(port,()=>{
-    console.log(`server is running on port ${port}`);
+app.listen(port,()=>{
+    console.log(`server is working on port ${port}`);
 })
