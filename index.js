d02df0608d908b0d60c73fa45e788eb3acc165e9
MU SALEH <mohamed.s.kamaal@gmail.com>
2023-10-02 12:24:29 +0300

    adding the logging middleware

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const express= require('express');
 
 const booksRouter = require('./routes/books.route')
 const authersRouter = require('./routes/authers.route')
+const logger = require('./middlewares/logger.js')
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 dotenv.config()
-
+ 
 mongoose
     .connect(process.env.MONGO_URI)
     .then(()=>console.log("Connected to MongoDB... "))
@@ -18,11 +19,11 @@ const app = express();
 
 //apply middlewares
 app.use(express.json());
+app.use(logger.logger)
 
 // Mount Routes
 app.use('/api/books',booksRouter)
 app.use('/api/authers',authersRouter)
-
 
 //Running The Server
 const port = process.env.PORT
