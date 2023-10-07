2189e7436228bae724f01ef1338a55f0b0a521ee
MU SALEH <mohamed.s.kamaal@gmail.com>
2023-10-07 21:35:18 +0300

    adding authContoller file

---
+++ controller/authersController.js
@@ -0,0 +1,134 @@
+const AutherModel = require('../models/auther.model.js')
+const asyncHandler = require('express-async-handler')
+const {validateCreateAuther,validateUpdateAuther} = require('../models/auther.model.js')
+
+/**
+ * @desc Get All authers
+ * @route /api/books
+ * @method Get
+ * @access Public
+ */
+const getAllAuthers=asyncHandler(async(req,res)=>{
+    try {
+        const authers= await AutherModel.find({}).sort({firstName:-1}).select("firstName lastName -_id")
+        res.status(200).json(authers);
+    } catch (error) {
+
+        console.log(error);
+        res.status(500).json({message:"something went wrong"})    
+    }
+   
+  })
+
+  /**
+ * @desc Get auther By ID
+ * @route /api/authers/:id
+ * @method Get
+ * @access Public
+ */
+  const getAutherById=asyncHandler(async(req,res)=>{
+    const {id}= req.params
+
+    try {
+        const auther = await AutherModel.findById(id)
+        res.status(200).json(auther)
+        
+    } catch (error) {
+
+        console.log(error);
+        res.status(404).json({message:"something went wrong"})
+        
+    }
+})
+/**
+ * @desc Create a New auther
+ * @route /api/authers
+ * @method post
+ * @access private (Only admins)
+ */
+const createAuther = asyncHandler(async(req,res)=>{
+    
+    const {error} =validateCreateAuther(req.body)
+      if (error) return res.status(404).json({ErorMessage:error.details[0].message})
+         
+    try {
+     const auther = new AutherModel ({
+         firstName:req.body.firstName,
+         lastName:req.body.lastName,
+         nationality:req.body.nationality,
+         image:req.body.image
+     })
+     const result = await auther.save();
+     res.status(200).json(result)
+         
+    } catch (error) {
+     console.log(error);
+     res.status(500).json({message:"something went wrong"})
+     
+    }
+     
+ })
+/**
+ * @desc Update an auther By ID
+ * @route /api/authers/:id
+ * @method PUT
+ * @access private (Only admins)
+ */
+const updateAuther = asyncHandler(async(req,res)=>{
+
+    const {error} = validateUpdateAuther(req.body)
+   if (error) {
+       return res.status(404).json({message:error.details[0].message})
+   }
+   const {id}= req.params
+   const {firstName}=req.body
+   const {lastName}=req.body
+
+
+   try {
+       console.log(id)
+       const auther = await AutherModel.findByIdAndUpdate(id,{firstName,lastName},{new:true})
+       res.status(200).json(auther)
+       
+   } catch (error) {
+
+       console.log(error);
+       res.status(404).json({message:"something went wrong"})
+       
+   }
+})
+
+/**
+ * @desc Delete an auther By ID
+ * @route /api/autheras/:id
+ * @method Delete
+ * @access private (Only admins)
+ */
+const deleteAuther = asyncHandler(async(req,res)=>{
+    const {error} = validateUpdateAuther(req.body)
+   if (error) {
+       return res.status(404).json({message:error.details[0].message})
+   }
+   const {id}= req.params
+
+   try {
+       const auther = await AutherModel.findByIdAndDelete(id)
+       res.status(200).json(auther)
+       
+   } catch (error) {
+
+       console.log(error);
+       res.status(404).json({message:"something went wrong"})
+       
+   }
+})
+
+
+module.exports={
+    getAllAuthers,
+    getAutherById,
+    createAuther,
+    updateAuther,
+    deleteAuther
+    
+}
