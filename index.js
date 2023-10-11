25ebbe11ff6b0170abd297338fdd158071985577
MU SALEH <mohamed.s.kamaal@gmail.com>
2023-10-11 21:33:19 +0300

    adding uploading files feature using multer

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ const express= require('express');
 const logger = require('./middlewares/logger.js')
 const {notFound,errorHandler} = require('./middlewares/errors')
 const dbConnection=require('./config/dbConnection')
+const path= require('path')
       require('dotenv').config()
 
 // Database Connection
 dbConnection()
 // Init The App
 const app = express();
+
+// static folder 
+app.use(express.static(path.join(__dirname,"images")));
 
 //apply middlewares
 app.set('view engine','ejs')
@@ -20,8 +24,10 @@ app.use('/api/books',  require('./routes/books'))
 app.use('/api/authers',require('./routes/authers'))
 app.use('/api/auth',   require ('./routes/auth'))
 app.use('/api/users',  require ('./routes/users'))
+app.use('/api/upload',  require ('./routes/upload'))
 app.use('/',  require ('./routes/resete-password'))
 
+// Error Handling
 app.use(notFound)
 app.use(errorHandler)
 
---
+++ routes/upload.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const router = express.Router()
+const path = require('path')
+const multer = require('multer')
+
+const storage = multer.diskStorage({
+    destination:function(req,file,cb){
+        cb(null,path.join(__dirname,'../images'))
+    },
+    filename:function(req,file,cb){
+        cb(null,new Date().toISOString().replace(/:/g,'-')+file.originalname)
+    }
+})
+const upload = multer({storage})
+router.post('/',upload.single('image'),(req,res)=>{
+    res.status(200).json({message:"image uploaded"})
+})
+
+module.exports=router
